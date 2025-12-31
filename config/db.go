ffa92b714518796fa9cff8f2413fda5c67b0805f
VIJAY RAMESH <101918879+Vijayramesh26@users.noreply.github.com>
2025-12-31 15:49:11 +0530

    Change database connection string in ConnectDB function
    Updated database connection string to use a new DSN.

--- config/db.go
+++ config/db.go
@@ -13,9 +13,10 @@ var DB *gorm.DB
 func ConnectDB() {
 
 	// Example using os.Getenv
-dsn := os.Getenv("DATABASE_URL")  
+// dsn := os.Getenv("DATABASE_URL")  
 	
 	// dsn := "root:root@tcp(localhost:3306)/kyc_demo?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "dbpgf21238876:D}ggV6AzHJLg2Tr7Q8KVz@tcp(serverless-us-east1.sysp0000.db2.skysql.com: 4014)/kyc_demo?charset=utf8mb4&parseTime=True&loc=Local"
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
